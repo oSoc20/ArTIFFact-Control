ff0a98a8a2aac663850b39f930c7c7487cd2a69e
Denis Christopher <la171126@student.helha.be>
2020-07-09 13:48:19 +0200

    sidebar code improvements

--- src/renderer/screens/Sidebar/Sidebar.tsx
+++ src/renderer/screens/Sidebar/Sidebar.tsx
@@ -1,11 +1,4 @@
 import * as React from 'react';
-// Resources
-import logoWithLabel from '../../../resources/images/logoWithLabel.svg';
-// Redux store
-import { connect } from 'react-redux';
-import { Dispatch } from 'redux';
-import { RootState } from '../../reducers';
-import { SidebarAction, setActiveItem } from '../../actions/SidebarAction';
 // Material UI
 import { Drawer, List, ListItem, ListItemText, ListItemIcon } from '@material-ui/core';
 import { makeStyles, Theme, createStyles } from '@material-ui/core/styles';
@@ -19,6 +12,8 @@ import HelpOutlineOutlinedIcon from '@material-ui/icons/HelpOutlineOutlined';
 import InfoOutlinedIcon from '@material-ui/icons/InfoOutlined';
 import { useHistory } from "react-router-dom";
 
+/* IMAGES */
+const logoWithLabel = require('../../resources/images/logoWithLabel.svg');
 
 /* STYLING */
 const drawerWidth = 260;
@@ -76,32 +71,21 @@ const bottomItems = [
 ];
 
 /* FUNCTIONS */
-function renderItems(items: any[], activeItem: string, setActiveItem: any, classes: any, history: any): any {
+function renderItems(items: any[], selectedItem: string, setSelectedItem: any, classes: any, history: any): any {
     return items.map((item, index) => (
-        <ListItem button key={index} classes={{ selected: classes.selected }} style={listItem} selected={activeItem == item.link} onClick={() => changeActiveItem(item.link, setActiveItem, history)}>
+        <ListItem button key={index} classes={{ selected: classes.selected }} style={listItem} selected={selectedItem == item.link} onClick={() => { setSelectedItem(item.link); history.push('/' + item.link); }}>
             {item.icon}
-            <ListItemText><span style={{fontSize:'18px'}}>{item.name}</span></ListItemText>
+            <ListItemText><span style={{ fontSize: '18px' }}>{item.name}</span></ListItemText>
         </ListItem>
     ));
 }
 
-function changeActiveItem(link: string, setActiveItem: any, history: any) {
-    setActiveItem(link);
-    history.push('/' + link);
-}
-
-/* INTERFACES */
-export interface Props {
-    activeItem: string;
-    setActiveItem: (value: string) => any;
-}
-
-
 /* COMPONENT */
-function Sidebar(props: Props) {
+function Sidebar() {
     const classes = useStyles();
     const history = useHistory();
-    
+    const [selectedItem, setSelectedItem] = React.useState("dashboard");
+
     return (
         <nav className={classes.drawer} aria-label="sidebar">
             <Drawer
@@ -118,25 +102,14 @@ function Sidebar(props: Props) {
                     </ListItem>
                 </List>
                 <List disablePadding dense style={{ flex: 1 }}>
-                    {renderItems(middleItems, props.activeItem, props.setActiveItem, classes, history)}
+                    {renderItems(middleItems, selectedItem, setSelectedItem, classes, history)}
                 </List>
                 <List disablePadding dense style={{ flex: 'none', marginBottom: '22px' }}>
-                    {renderItems(bottomItems, props.activeItem, props.setActiveItem, classes, history)}
+                    {renderItems(bottomItems, selectedItem, setSelectedItem, classes, history)}
                 </List>
             </Drawer>
         </nav>
     );
 };
 
-
-/* REDUX STORE */
-
-const mapStateToProps = (state: RootState) => ({
-    activeItem: state.sidebar.activeItem
-});
-
-const mapDispatchToProps = (dispatch: Dispatch<SidebarAction>) => ({
-    setActiveItem: (value: string) => dispatch(setActiveItem(value))
-});
-
-export default connect(mapStateToProps, mapDispatchToProps)(Sidebar);
+export default (Sidebar);
