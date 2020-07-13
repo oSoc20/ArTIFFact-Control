62f1ccff3e4a43f7db318d6cf5b6c5da6f11048e
Freek De Sagher (win) <freek.de.sagher21@gmail.com>
2020-07-13 13:18:48 +0200

    Create file maniplulation actions

--- src/renderer/actions/FileCheckActions.ts
+++ src/renderer/actions/FileCheckActions.ts
@@ -2,12 +2,18 @@ import { Action, ActionCreator } from 'redux';
 
 /* Action types */
 
+// Stepper
 export const STEP_PROGRESS = 'STEP_PROGRESS';
 export const STEP_RESET = 'STEP_RESET';
+
+// Files
+export const CLEAR_FILES = 'CLEAR_FILES';
+export const SET_FILES = 'SET_FILES';
 
 
 /* Typescript interfaces */
 
+// Stepper
 export interface StepProgressAction extends Action {
     type: 'STEP_PROGRESS';
 }
@@ -16,8 +22,20 @@ export interface StepResetAction extends Action {
     type: 'STEP_RESET';
 }
 
+// Files
+export interface SetFilesAction extends Action {
+    type: 'SET_FILES';
+    files: Array<Object>;
+}
+
+export interface ClearFilesAction extends Action {
+    type: 'CLEAR_FILES';
+}
+
 
 /* Action functions*/
+
+// Stepper
 
 /**
  * Progress one step. Increases the counter by 1
@@ -34,5 +52,23 @@ export const resetStep: ActionCreator<StepResetAction> =  () => ({
 });
 
 
+// Files
+
+/**
+ * Clear the list of files
+ */
+export const clearFiles: ActionCreator<ClearFilesAction> = () => ({
+    type: CLEAR_FILES,
+});
+
+/**
+ * Set the list of files to a new list of files
+ */
+export const setFiles: ActionCreator<SetFilesAction> = (files: Array<Object>) => ({
+    type: SET_FILES,
+    files: files
+});
+
+
 // Type that references each possible action type
-export type FilecheckAction = StepProgressAction | StepResetAction;
+export type FilecheckAction = StepProgressAction | StepResetAction | ClearFilesAction | SetFilesAction;
