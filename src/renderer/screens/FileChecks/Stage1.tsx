5d0062692ae955db86cc00515341ff731e4d5ceb
Freek De Sagher (win) <freek.de.sagher21@gmail.com>
2020-07-13 11:26:06 +0200

    Create basic flow for file selection

--- src/renderer/screens/FileChecks/Stage1.tsx
+++ src/renderer/screens/FileChecks/Stage1.tsx
@@ -1,5 +1,97 @@
 import * as React from 'react';
+import { useState } from 'react'
+import { makeStyles, Theme, createStyles } from '@material-ui/core/styles';
+import { Box, Container, TableContainer, TableCell, TableHead, TableBody, TableRow } from '@material-ui/core'
+import {default as DeleteIcon } from '@material-ui/icons/DeleteForever';
+
+
+interface FileData {
+    path: string;
+    size: string;
+}
+
+
+const useStyles = makeStyles((theme: Theme) => 
+    createStyles({
+      container: {
+          background: "#eee"
+      },
+      fileContainer: {
+          border: "2px dashed grey",
+          borderRadius: "5px",
+          margin: "5rem"
+      },
+      centered: {
+          marginLeft: "auto",
+          marginRight: "auto"
+      }
+    })
+);
+
 
 export const Stage1 = () => {
-    return <div>Stage 1 rendering here</div>
+    const classes = useStyles();
+
+    let [files, setFiles] = useState<Array<FileData>>([])
+
+    const hasFiles = () => {
+        return files.length !== 0;
+    }
+
+    const removeFile = (index: number) => {
+        // React state only updates if setFiles callback is called
+        // Therefore we create a copy of the current array, slice it and
+        // then update using setFiles callback
+        let filesCopy = [...files];
+        filesCopy.splice(index, 1);
+        setFiles(filesCopy);
+    }
+
+    const addSampleFiles = () => {
+        const files = [ {path: "C://TEST/file.tiff", size: "300GB"}, {path: "C://TEST//file2.tiff", size: "3kb"}];
+        setFiles(files);
+    }
+
+    return (
+        <Container>
+            <h3>Step 1 - File upload</h3>
+            <Box className={classes.fileContainer} >
+                {!hasFiles()? 
+                <>
+                    <p>Drop .TIFF files here</p>
+                    <button onClick={() => addSampleFiles()}>Upload your files</button>    
+                </> 
+                : 
+                <> 
+                    <TableContainer>
+                        <TableHead>
+                            <TableRow>
+                                <TableCell>Path</TableCell>
+                                <TableCell>size</TableCell>
+                                <TableCell/>
+                            </TableRow>
+                        </TableHead>
+                        <TableBody>
+                            {files.map((file, index) => {
+                               return (
+                                   <TableRow key={index}>
+                                       <TableCell>{file.path}</TableCell>
+                                       <TableCell>{file.size}</TableCell>
+                                       <TableCell>
+                                           <button style={{background: "none", border: "none"}}
+                                                    onClick={() => removeFile(index)}
+                                           ><DeleteIcon/></button>
+                                        </TableCell>
+                                   </TableRow>
+                               ); 
+                            })}
+                        </TableBody>
+                    </TableContainer>
+                    <small>+ add items</small>
+                </>
+                }
+                
+            </Box>
+        </Container>
+    );
 }
