c5598bb3bb1f0243e1dfef2ca74c6f194681a68f
Denis Christopher <la171126@student.helha.be>
2020-07-16 12:00:43 +0200

    Clear options popup
    custom date picker

--- src/renderer/components/ReportsTable/ReportsTable.tsx
+++ src/renderer/components/ReportsTable/ReportsTable.tsx
@@ -1,12 +1,15 @@
 import * as React from 'react';
 // Material UI
-import { Paper, makeStyles, Theme, createStyles, TableContainer, Table, TableHead, TableRow, withStyles, TableBody, Button } from '@material-ui/core';
+import { Paper, makeStyles, Theme, createStyles, TableContainer, Table, TableHead, TableRow, withStyles, TableBody, Button, Popper, Fade, Typography, PopperPlacementType, FormControlLabel, Radio, Grid, RadioGroup } from '@material-ui/core';
 import MuiTableCell from "@material-ui/core/TableCell";
+// Icons
+import EventNoteIcon from '@material-ui/icons/EventNote';
 import CheckIcon from '@material-ui/icons/Check';
 import ClearIcon from '@material-ui/icons/Clear';
-// Icons
+import CloseIcon from '@material-ui/icons/Close';
 import DeleteBinIcon from 'Assets/icons/icons8-delete-bin-500.svg';
 import ClearOptionIcon from 'Assets/icons/icons8-clear-option-500.svg';
+import CustomDatePicker from 'Components/CustomDatePicker/CustomDatePicker';
 
 /* STYLE */
 const TableCell = withStyles({
@@ -45,6 +48,20 @@ const useStyles = makeStyles((theme: Theme) =>
         tableHeadCell: {
             color: '#39657B',
             fontWeight: 600
+        },
+        popup: {
+            padding: theme.spacing(2),
+            color: 'black',
+            background: '#FCFCFC',
+            boxShadow: '0px 0px 19px rgba(0, 0, 0, 0.05)',
+            borderRadius: '12px',
+        },
+        closeIcon: {
+            color: theme.palette.grey[100],
+            "&:hover": {
+                color: 'black',
+                cursor: 'pointer'
+            }
         }
     })
 );
@@ -58,64 +75,118 @@ interface ReportsTableProps {
 /* COMPONENT */
 const ReportsTable = (props: ReportsTableProps) => {
     const classes = useStyles();
+    const [anchorEl, setAnchorEl] = React.useState<HTMLButtonElement | null>(null);
+    const [open, setOpen] = React.useState(false);
+    const [placement, setPlacement] = React.useState<PopperPlacementType>();
+    const [value, setValue] = React.useState('clearAll');
+    const [selectedDate, setSelectedDate] = React.useState<Date | null>(
+        new Date()
+    );
 
-    return (
-        <>
-            <Paper className={classes.paper}>
-                <TableContainer style={{ height: '450px', overflow: "auto" }} >
-                    <Table aria-label="span" size="small" stickyHeader>
-                        <TableHead>
-                            <TableRow className={classes.tableHeadRow}>
-                                <TableCell className={classes.tableHeadCell}>Date</TableCell>
-                                <TableCell className={classes.tableHeadCell}>Files</TableCell>
-                                <TableCell className={classes.tableHeadCell}>Input</TableCell>
-                                <TableCell className={classes.tableHeadCell}>Result</TableCell>
-                                <TableCell className={classes.tableHeadCell}>Errors</TableCell>
-                                <TableCell className={classes.tableHeadCell}>Passed</TableCell>
-                                <TableCell className={classes.tableHeadCell}>Score</TableCell>
-                                <TableCell className={classes.tableHeadCell}></TableCell>
-                            </TableRow>
-                        </TableHead>
-                        <TableBody>
-                            {props.reports.map((report, index) => {
-                                return (
-                                    <StyledTableRow key={index}>
-                                        <TableCell component="th" scope="row">
-                                            {report.date.toLocaleDateString()}
-                                        </TableCell>
-                                        <TableCell component="th" scope="row">
-                                            {report.files}
-                                        </TableCell>
-                                        <TableCell component="th" scope="row">
-                                            {report.input}
-                                        </TableCell>
-                                        <TableCell component="th" scope="row">
-                                            {report.result ? <CheckIcon style={{ color: 'green' }} /> : <ClearIcon style={{ color: 'red' }} />}
-                                        </TableCell>
-                                        <TableCell component="th" scope="row">
-                                            {report.errors} errors
-                                        </TableCell>
-                                        <TableCell component="th" scope="row">
-                                            {report.passed} Passed
-                                        </TableCell>
-                                        <TableCell component="th" scope="row">
-                                            {report.score}%
-                                        </TableCell>
-                                        <TableCell component="th" scope="row">
-                                            <Button onClick={() => props.removeReports(index)}><img src={DeleteBinIcon} style={{ width: '24px' }} /></Button>
-                                        </TableCell>
-                                    </StyledTableRow>
-                                );
-                            })}
-                        </TableBody>
-                    </Table>
-                </TableContainer>
-                <div style={{ width: '100%', textAlign: 'right' }}>
-                    <Button style={{ marginTop: '20px', fontSize: '14px', textTransform: 'none' }}><img src={ClearOptionIcon} style={{ width: '20px', marginRight: '8px' }} /> Clear options</Button>
-                </div>
-            </Paper>
-        </>
-    )
+    const handleClick = (newPlacement: PopperPlacementType) => (
+        event: React.MouseEvent<HTMLButtonElement>,
+    ) => {
+        setAnchorEl(event.currentTarget);
+        setOpen((prev) => placement !== newPlacement || !prev);
+        setPlacement(newPlacement);
+    };
+
+    const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setValue((event.target as HTMLInputElement).value);
+    };
+
+    const handleClear = () => {
+        setOpen(false)
+    }
+
+    return <>
+        <Paper className={classes.paper}>
+            <TableContainer style={{ height: '450px', overflow: "auto" }} >
+                <Table aria-label="span" size="small" stickyHeader>
+                    <TableHead>
+                        <TableRow className={classes.tableHeadRow}>
+                            <TableCell className={classes.tableHeadCell}>Date</TableCell>
+                            <TableCell className={classes.tableHeadCell}>Files</TableCell>
+                            <TableCell className={classes.tableHeadCell}>Input</TableCell>
+                            <TableCell className={classes.tableHeadCell}>Result</TableCell>
+                            <TableCell className={classes.tableHeadCell}>Errors</TableCell>
+                            <TableCell className={classes.tableHeadCell}>Passed</TableCell>
+                            <TableCell className={classes.tableHeadCell}>Score</TableCell>
+                            <TableCell className={classes.tableHeadCell}></TableCell>
+                        </TableRow>
+                    </TableHead>
+                    <TableBody>
+                        {props.reports.map((report, index) => {
+                            return (
+                                <StyledTableRow key={index}>
+                                    <TableCell component="th" scope="row">
+                                        {report.date.toLocaleDateString()}
+                                    </TableCell>
+                                    <TableCell component="th" scope="row">
+                                        {report.files}
+                                    </TableCell>
+                                    <TableCell component="th" scope="row">
+                                        {report.input}
+                                    </TableCell>
+                                    <TableCell component="th" scope="row">
+                                        {report.result ? <CheckIcon style={{ color: 'green' }} /> : <ClearIcon style={{ color: 'red' }} />}
+                                    </TableCell>
+                                    <TableCell component="th" scope="row">
+                                        {report.errors} errors
+                                    </TableCell>
+                                    <TableCell component="th" scope="row">
+                                        {report.passed} Passed
+                                    </TableCell>
+                                    <TableCell component="th" scope="row">
+                                        {report.score}%
+                                    </TableCell>
+                                    <TableCell component="th" scope="row">
+                                        <Button onClick={() => props.removeReports(index)}><img src={DeleteBinIcon} style={{ width: '24px' }} /></Button>
+                                    </TableCell>
+                                </StyledTableRow>
+                            );
+                        })}
+                    </TableBody>
+                </Table>
+            </TableContainer>
+            <div style={{ width: '100%', textAlign: 'right' }}>
+                <Button style={{ marginTop: '20px', fontSize: '14px', textTransform: 'none' }} onClick={handleClick("left")}><img src={ClearOptionIcon} style={{ width: '20px', marginRight: '8px' }} /> Clear options</Button>
+            </div>
+            <Popper open={open} anchorEl={anchorEl} placement={placement} style={{ marginRight: '5px' }} transition>
+                {({ TransitionProps }) => (
+                    <Fade {...TransitionProps} timeout={200}>
+                        <Paper className={classes.popup}>
+                            <Grid container>
+                                <Grid item xs={12} style={{ textAlign: 'right' }}>
+                                    <CloseIcon className={classes.closeIcon} style={{ width: '22px' }} onClick={() => setOpen(false)} />
+                                </Grid>
+                                <Grid item xs={12}>
+                                    <RadioGroup aria-label="gender" name="gender1" value={value} onChange={handleChange}>
+                                        <FormControlLabel value="clearAll" control={
+                                            <Radio color="primary" />
+                                        } label={
+                                            <Typography>Clear all</Typography>
+                                        } />
+                                        <FormControlLabel value="olderThan" control={
+                                            <Radio color="primary" value='' />
+                                        } label={
+                                            <div style={{ display: 'flex', alignItems: 'center' }}>
+                                                <Typography style={{ marginRight: '10px' }}>Older than</Typography>
+                                                <div style={{ width: '160px' }}><CustomDatePicker selectedDate={selectedDate} setSelectedDate={setSelectedDate} /></div>
+                                            </div>
+                                        } />
+                                    </RadioGroup>
+                                </Grid>
+                                <Grid item xs={12} style={{ textAlign: 'right', marginTop: '10px' }}>
+                                    <Button variant="contained" color="primary" style={{ textTransform: 'none', borderRadius: '5px' }} onClick={handleClear}>Clear</Button>
+                                </Grid>
+                            </Grid>
+                        </Paper>
+                    </Fade>
+                )}
+            </Popper>
+        </Paper>
+    </>
 }
 
 export default (ReportsTable);
